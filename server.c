c9324c1607ebd987034b94cb983efbf183130b84
oaoi77 <dbnhi361@gmail.com>
2024-11-29 12:37:42 +0700

    Create server.c and create function to receive file from client

---
+++ server.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define BUFFER_SIZE 1024
+
+//Function to receive file from client 
+//server receive the data from client and write to file
+void recv_File(){
+    int n;
+    FILE *fp;
+    char *fn = "message.txt";
+    char buffer[size];
+
+    fp = fopen(fn, "w");
+    //loop until connection closed or error occur
+    while (1) {
+        //recv return the number of bytes read
+        n = recv(sockfd, buffer, BUFFER_SIZE, 0)
+        if (n<=0){
+            break;
+            return;
+        }
+        //print the data received to message.txt
+        fprintf(fp, "%s", buffer);
+        bzero(buffer, BUFFER_SIZE); //Clear all data in buffer
+    }
+    return;
+}
+
+
+
