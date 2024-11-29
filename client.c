632473d657803b6cecf03bc22e210a526b8d0ec0
oaoi77 <dbnhi361@gmail.com>
2024-11-29 23:14:04 +0700

    Create client.c for client side

---
+++ client.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd/h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "127.0.0.1" //localhost
+#define SERVER_PORT 8080
+#define BUFFER_SIZE 1024
+
+
