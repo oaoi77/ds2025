c33b9d746a9ffee18681cde3e5088e4b5301ef3b
oaoi77 <dbnhi361@gmail.com>
2024-11-29 23:26:48 +0700

    Create function to send file from client to server

--- client.c
+++ client.c
@@ -7,4 +7,16 @@
 #define SERVER_PORT 8080
 #define BUFFER_SIZE 1024
 
+void send_file(FILE *fp, int server_socket){
+    int n;
+    char buffer[BUFFER_SIZE];
+    while(fgets(buffer, BUFFER_SIZE, fp) != NULL){
+        if(send(server_socket, buffer, sizeof(buffer), 0) == -1){
+            perror("[-] Sending file failed")
+            exit(EXIT_FAILURE);
+        }
+        bzero(buffer, BUFFER_SIZE);
+    }
+}
+
 
